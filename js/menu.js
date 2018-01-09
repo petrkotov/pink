cae1f36adc2a4ffdbcd858fe167af41fca4dab93
petrkotov <30962854+petrkotov@users.noreply.github.com>
2018-01-09 03:48:54 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,23 @@
-var navMain = document.querySelector(".main-nav");
+var navMain = document.querySelector(".main-nav__list");
 var navToggle = document.querySelector(".main-nav__toggle");
-var navLogin = document.querySelector(".main-nav__user-login");
-var navShow = document.querySelector(".modal-login");
-var navClose = document.querySelector(".modal-login__close");
-navMain.classList.remove("main-nav--nojs");
+var navBurger1 = document.querySelector(".main-nav__toggle--burger1");
+var navBurger2 = document.querySelector(".main-nav__toggle--burger2");
+var navBurger3 = document.querySelector(".main-nav__toggle--burger3");
+
 navToggle.addEventListener("click", function(){
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
+  if (navMain.classList.contains("main-nav__list--closed")) {
+    navMain.classList.remove("main-nav__list--closed");
+    navMain.classList.add("main-nav__list--opened");
+    navBurger1.classList.add("main-nav__toggle--pressed1");
+    navBurger2.classList.add("main-nav__toggle--pressed2");
+    navBurger3.classList.add("main-nav__toggle--pressed3");
   }
   else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav__list--closed");
+    navMain.classList.add("main-nav__list--closing");
+    navMain.classList.remove("main-nav__list--opened");
+    navBurger1.classList.remove("main-nav__toggle--pressed1");
+    navBurger2.classList.remove("main-nav__toggle--pressed2");
+    navBurger3.classList.remove("main-nav__toggle--pressed3");
   }
-})
-
-navLogin.addEventListener("click", function(){
-    navShow.classList.add("modal-login--show");
-})
-
-navClose.addEventListener("click", function(){
-    navShow.classList.remove("modal-login--show");
 })
